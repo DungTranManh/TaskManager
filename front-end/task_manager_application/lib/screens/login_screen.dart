7fb79fd1dc4f314f9be9b2d13862e0a2e296c9d1
DESKTOP-F4DDIQ8\ahihi <manhdung5801@gmail.com>
2022-06-06 22:57:47 +0700

    update login screen and add font family- OpenSans

--- front-end/task_manager_application/lib/screens/login_screen.dart
+++ front-end/task_manager_application/lib/screens/login_screen.dart
@@ -12,8 +12,50 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Text("Login Screen"),
+        child: Container(
+          color: Color.fromARGB(190, 33, 149, 243),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50.0,
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: Text(
+                  "Sign In",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontFamily: "OpenSans",
+                  ),
+                ),
+                height: 55.0,
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                padding: EdgeInsets.only(left: 30.0),
+                child: Text(
+                  "Email:",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15.0,
+                    fontFamily: "OpenSans",
+                  ),
+                ),
+                height: 55.0,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                color: Colors.blue,
+                child: TextField(),
+              ),
+            ],
+          ),
         ),
       ),
     );
